1f8d85fe85354e4d4a0861cee8a69020dedb054e
fedrick neto <fedrickpneto123@gmail.com>
2020-06-29 11:29:12 +0530

    Complete Keepre App

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+    const y = new Date().getFullYear();
+    return(
+        <footer>
+            <p>Copyright 🤍 {y} </p>
+        </footer>
+    )
+}
+
+export default Footer;
